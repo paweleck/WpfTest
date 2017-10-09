859e399f9339ef7242d2eeed5388d9e6d2ea846d
paweleck <czemierysp@op.pl>
2017-10-09 17:12:13 +0200

    init

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,45 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            ListDirectory(tree, "D:\\STI");
+        }
+
+        private void GridSplitter_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+
+        }
+
+        private void ListDirectory(TreeView treeView, string path)
+        {
+            treeView.Items.Clear();
+            var rootDirectoryInfo = new DirectoryInfo(path);
+            treeView.Items.Add(CreateDirectoryNode(rootDirectoryInfo));
+        }
+
+        private static TreeViewItem CreateDirectoryNode(DirectoryInfo directoryInfo)
+        {
+            var directoryNode = new TreeViewItem { Header = directoryInfo.Name };
+            foreach (var directory in directoryInfo.GetDirectories())
+                directoryNode.Items.Add(CreateDirectoryNode(directory));
+
+            foreach (var file in directoryInfo.GetFiles())
+                directoryNode.Items.Add(new TreeViewItem { Header = file.Name });
+
+            return directoryNode;
+        }
+
+        private void TreeViewControl_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            if (e.NewValue is TreeViewItem)
+            {
+                TreeViewItem item = e.NewValue as TreeViewItem;
+                selectedTreeItem.Content = item.Header;
+            }
+            else
+            {
+                //if the user tries to select an object that isn't an Item (i.e. a group) reselect the first Item in that group
+                //This will then cause stack overflow in methods I've tried so far
+            }
         }
     }
 }
